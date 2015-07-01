e6fadf714d28f91c37901f7802bcac161cf5910c
a564522 <a564522@MC0QPEUC.ww930.my-it-solutions.net>
2015-07-01 14:26:36 -0300

    commit

---
+++ src/br/com/helperhs/control/FXMLTesteController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.helperhs.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author a564522
+ */
+public class FXMLTesteController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
